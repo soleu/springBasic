499b451bd9cd18a9b5670c740c882ce0633674c0
soleu <dlthf555@gmail.com>
2021-08-21 23:26:33 +0900

    DI 주입 코드 변경

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,9 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp { //순수 자바로 테스트 하는 방법
     public static void main(String[] args) {
-        MemberService memberService=new MemberServiceImpl();
+        AppConfig appConfig=new AppConfig();
+        // MemberService memberService=new MemberServiceImpl();
+        MemberService memberService= appConfig.memberService();//이런식으로 appConfig를 이용하여 Service 메서드를 사용한다
         Member member = new Member(1L,"memberA",Grade.VIP);
         memberService.join(member);
 
