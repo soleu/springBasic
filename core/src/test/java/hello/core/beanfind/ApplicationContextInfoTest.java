5babbc2753ec70fa28ec9ffd9bc4858029a32020
soleu <dlthf555@gmail.com>
2021-09-03 23:57:24 +0900

    add

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -21,14 +21,15 @@ public class ApplicationContextInfoTest {
     }
 
     @Test
-    @DisplayName("애플리케이션 빈 출력하기")
+    @DisplayName("애플리케이션 빈 출력하기") //내가 짠 코드만 보기
     void findApplicationBean(){
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();//ctrl + alt + v
         for (String beanDefinitionName : beanDefinitionNames) { //iter +tab : for문 자동완성
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);//beanDefinition : 각각의 bean에 관한 정보
 
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
-            //Role ROLE_INn
-            if(beanDefintion.getRole()= BeanDefinition.ROLE_APPLICATION){
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){ //getRole() : 내가 사용하기 위해 등록한 빈만 출력
              Object bean =ac.getBean(beanDefinitionName);
                 System.out.println("name = "+beanDefinitionName + "object= "+bean);
             }
