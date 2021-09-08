1f7d11d8cb2876325cc446254051889ff6740289
sol <dlthf555@gmail.com>
2021-09-08 20:22:54 +0900

    singleton

---
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    //자기자신을 내부의 static으로 선언
+    private  static final SingletonService instance = new SingletonService();
+    //조회
+    public  static SingletonService getInstance(){
+        return instance;
+    }
+
+    
+    public static void main(String[] args){
+
+    }
+
+}
