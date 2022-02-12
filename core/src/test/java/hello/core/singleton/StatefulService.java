d98a16c9859148d451a13fc4ba10ac0a8ff7f419
soleu <dlthf555@gmail.com>
2022-02-13 00:37:21 +0900

    add : stateless로 컨테이너 사용하기

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -1,11 +1,13 @@
 package hello.core.singleton;
+
 //cmd+shift+T
 public class StatefulService {
     private int price;
 
-    public static void order(String name,int price) {
-        System.out.println("name = " + name +"price = "+price);
-        this.price=price;//
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+//        this.price=price; //여기서 값을 변경 X
+        return price;
     }
 
     public int getPrice() {
