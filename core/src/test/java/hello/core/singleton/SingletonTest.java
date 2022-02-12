bf85d7cc37114cd5f32d9b069951f221291e55fd
soleu <dlthf555@gmail.com>
2022-02-12 23:45:00 +0900

    add : 싱글톤 컨테이너 적용

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,13 +14,13 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContainer(){
-        AppConfig appConfig=new AppConfig();
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
         //1.조회 : 호출할 때 마다 객체를 생성
-        MemberService memberService1=appConfig.memberService();
+        MemberService memberService1 = appConfig.memberService();
 
         //2.조회 : 호출할 때 마다 객체를 생성
-        MemberService memberService2=appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
 
         //참조값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
@@ -29,15 +31,33 @@ public class SingletonTest {
         //고객의 요청이 들어올때(트래픽이 발생할 때 마다 객체가 생성됨)
 
     }
+
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
-        SingletonService singletonService1=SingletonService.getInstance();
-        SingletonService singletonService2=SingletonService.getInstance();
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
 //       soutv + tab
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
         //isSameAs : 인스턴스가 같은지, isEqualsAs : 자바의 equals
         assertThat(singletonService1).isSameAs(singletonService2); //싱글톤으로 같은 객체 인스턴스를 반환(new X)
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig=new AppConfig();
+        //스프링 컨테이너 생성
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
