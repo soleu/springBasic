63ff15687573463b3fb3d3bcebbad0a8c64de989
soleu <dlthf555@gmail.com>
2022-02-20 00:10:53 +0900

    add : 프록시로 웹 요청 추가

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;//provider로 대신 주입
+    private final MyLogger myLogger;//provider로 대신 주입
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger=myLoggerProvider.getObject();
+//        MyLogger myLogger=myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
